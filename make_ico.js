e8674ebcea26301c109efbbee8ff05cb94237837
ErenBelketin <erenbelketin@gmail.com>
2026-06-23 14:55:33 +0300

    Generate fully compliant multi-size Windows icon using png-to-ico and bump version to 1.1.2

--- make_ico.js
+++ make_ico.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const pngToIco = require('png-to-ico').default;
 
 const pngPath = path.join(__dirname, 'app_icon.png');
 const icoPath = path.join(__dirname, 'app_icon.ico');
@@ -9,25 +10,12 @@ if (!fs.existsSync(pngPath)) {
   process.exit(1);
 }
 
-const pngBuffer = fs.readFileSync(pngPath);
-const pngSize = pngBuffer.length;
-
-const icoHeader = Buffer.alloc(22);
-// Header
-icoHeader.writeUInt16LE(0, 0); // Reserved
-icoHeader.writeUInt16LE(1, 2); // Type 1 (Icon)
-icoHeader.writeUInt16LE(1, 4); // Number of images (1)
-
-// Directory Entry
-icoHeader.writeUInt8(0, 6); // Width (256)
-icoHeader.writeUInt8(0, 7); // Height (256)
-icoHeader.writeUInt8(0, 8); // Color palette
-icoHeader.writeUInt8(0, 9); // Reserved
-icoHeader.writeUInt16LE(1, 10); // Color planes
-icoHeader.writeUInt16LE(32, 12); // Bits per pixel (32)
-icoHeader.writeUInt32LE(pngSize, 14); // Size of PNG data
-icoHeader.writeUInt32LE(22, 18); // Offset (Header + Directory Entry = 22 bytes)
-
-const icoBuffer = Buffer.concat([icoHeader, pngBuffer]);
-fs.writeFileSync(icoPath, icoBuffer);
-console.log('Successfully created app_icon.ico');
+pngToIco(pngPath)
+  .then(buf => {
+    fs.writeFileSync(icoPath, buf);
+    console.log('Successfully created multi-size app_icon.ico');
+  })
+  .catch(err => {
+    console.error('Error generating ICO:', err);
+    process.exit(1);
+  });
